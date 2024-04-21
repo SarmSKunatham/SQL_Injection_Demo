3b2e03e144738a28eaec2f8678051e7a21aa28cc
tontan2545 <tontan2545@gmail.com>
2024-04-21 14:37:02 +0700

    feat: explicit error message

--- app/api/raw/product-search/route.ts
+++ app/api/raw/product-search/route.ts
@@ -5,14 +5,22 @@ export const GET = async (req: NextRequest) => {
   const searchParams = req.nextUrl.searchParams;
 
   const query = searchParams.get("query");
+  try {
+    if (!query) {
+      const products = await sql.unsafe(`SELECT * FROM products LIMIT 10`);
+      return NextResponse.json(products);
+    }
 
-  if (!query) {
-    const products = await sql.unsafe(`SELECT * FROM products LIMIT 10`);
+    const products = await sql.unsafe(
+      `SELECT * FROM products WHERE name ILIKE ${query} LIMIT 10`
+    );
     return NextResponse.json(products);
+  } catch (e: any) {
+    return NextResponse.json(
+      { error: e.message },
+      {
+        status: 400,
+      }
+    );
   }
-
-  const products = await sql.unsafe(
-    `SELECT * FROM products WHERE name ILIKE ${query} LIMIT 10`
-  );
-  return { products };
 };
