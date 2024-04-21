e8dc32f452533a641e08582d1648a585fbe46ffb
Sueptrakul Kunatham [Sarm] <87699705+SarmSKunatham@users.noreply.github.com>
2024-04-21 16:56:52 +0700

    feat(login-page): re design login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Link from 'next/link';
 import React from 'react';
 import { toast } from 'react-toastify';
 
@@ -72,32 +73,36 @@ const Login = () => {
 	}
 	return (
 		<section className='w-full h-[100vh] flex flex-col items-center justify-center'>
-			<div
-				style={{
-					backgroundColor: 'rgba(255, 255, 255, 0.49)',
-				}}
-				className='w-[50vw] h-[50vh] flex flex-col items-center justify-center backdrop-blur-lg rounded'
-			>
-				<h1 className='text-4xl font-bold text-black py-4'>Login</h1>
-				<form onSubmit={handleSubmit}>
-					<div>
-						<input
-							className='text-black'
-							type='text'
-							name='username'
-							placeholder='Username'
-						/>
+			<div className='w-[80vw] h-[50vh] rounded-xl bg-slate-600/25 shadow-lg border-2 border-slate-300 backdrop-blur-md text-center flex flex-col justify-center items-center space-y-8'>
+				<h1 className='text-3xl font-bold'>Login</h1>
+				<form onSubmit={handleSubmit} className='w-full px-20'>
+					<input
+						className='text-black text-lg w-full p-4 mb-4 rounded bg-transparent'
+						type='text'
+						name='username'
+						placeholder='Username'
+					/>
+					<input
+						className='text-black text-lg w-full p-4 rounded bg-none'
+						type='password'
+						name='password'
+						placeholder='Password'
+					/>
+					<div className='w-full flex justify-center mt-6'>
+						<button
+							className='text-lg w-48 border-2 border-white h-10 flex items-center justify-center rounded-full hover:bg-white/25 transition-colors duration-200"'
+							type='submit'
+						>
+							Login
+						</button>
 					</div>
-					<div>
-						<input
-							className='text-black'
-							type='password'
-							name='password'
-							placeholder='Password'
-						/>
-					</div>
-					<button type='submit'>Login</button>
 				</form>
+				<Link
+					href='/'
+					className='hover:text-white/25 transition-colors duration-200'
+				>
+					Back to home
+				</Link>
 			</div>
 		</section>
 	);
